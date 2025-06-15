86053f2c00bc39aa671d218e40f3c1c1149b42bd
ethancpizarro <ethancpizarro2@outlook.com>
2025-06-15 12:27:20 -0400

    Added followersCount files

---
+++ followersCount/followers_count.js
@@ -0,0 +1,25 @@
+let count = 0; // initialize count to 0
+
+function increaseCount()
+{
+    ++count; // increment the count by 1
+    displayCount(); // display the count
+    checkCountValue(); // check count value and display messages
+}
+
+function displayCount()
+{
+    document.getElementById("countDisplay").innerHTML = count; // display the count in the HTML
+}
+
+function checkCountValue()
+{
+    if (count === 10)
+    {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    }
+    else if (count === 20)
+    {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
